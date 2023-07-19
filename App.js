0a3c2194cd87a7646334abfc9052346a9ee28f2a
EvaGraceSmith <smithevagrace@gmail.com>
2023-07-19 01:53:01 -0500

    added styling

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
 import React from 'react';
 import { View, StyleSheet, KeyboardAvoidingView } from 'react-native';
+import { Box, NativeBaseProvider } from 'native-base';
+import { LinearGradient } from 'expo-linear-gradient';
 import ChatScreen from './screens/ChatScreen';
 import Header from './Components/Header';
 
 
+const config = {
+  dependencies: {
+    'linear-gradient': LinearGradient
+  },
+};
+
 const App = () => {
   return (
+    <NativeBaseProvider config={config}>
+            <Box
+        safeArea
+        h="100%"
+        bg={{
+          linearGradient: {
+            colors: ['amber.700', 'primary.600'],
+            start: [0, 0],
+            end: [1, 0],
+          },
+        }}
+      >
     <View style={styles.container}>
       <Header />
       <KeyboardAvoidingView
         style={styles.container}
         behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
       >
+
       <ChatScreen />
       </KeyboardAvoidingView>
     </View>
+    </Box>
+    </NativeBaseProvider>
   );
 };
 
